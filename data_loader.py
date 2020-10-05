8bb78e0a7f248d9d46d46ba2c081cef5e5b8d77d
Vincenzo Santopietro <vinsantopietro@gmail.com>
2020-10-05 17:17:31 +0200

    added frames & improved data_loader

--- data_loader.py
+++ data_loader.py
@@ -14,7 +14,7 @@ class DataLoader:
         self.batch_size = batch_size
 
         train_data_dir = pathlib.Path(self.training_data_path[:-1])
-        test_data_dir = pathlib.Path(self.test_data_path)
+        val_data_dir = pathlib.Path(self.test_data_path)
 
         self.image_shape = image_shape
 
@@ -27,15 +27,28 @@ class DataLoader:
             batch_size=self.batch_size
         )
 
-        #normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
-        #self.train_ds = self.train_ds.map(lambda x, y: (normalization_layer(x), y))
+        self.val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            val_data_dir,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(self.image_shape[1], self.image_shape[0]),
+            batch_size=self.batch_size)
+
+        normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
+        self.train_ds = self.train_ds.map(lambda x, y: (normalization_layer(x), y))
+        self.val_ds = self.train_ds.map(lambda x, y: (normalization_layer(x), y))
 
         # optimizing performances
-        #autotune = tf.data.experimental.AUTOTUNE
-        #self.train_ds = self.train_ds.cache().prefetch(buffer_size=autotune)
+        autotune = tf.data.experimental.AUTOTUNE
+        self.train_ds = self.train_ds.cache().prefetch(buffer_size=autotune)
+        self.val_ds = self.train_ds.cache().prefetch(buffer_size=autotune)
 
     def get_train_dataset(self):
         return self.train_ds
+
+    def get_val_dataset(self):
+        return self.val_ds
 
     def get_classes(self):
         return self.classes
--- main.py
+++ main.py
@@ -10,12 +10,14 @@ import argparse
 
 def main(arguments):
     # Use a breakpoint in the code line below to debug your script.
-    loader = DataLoader(arguments.dataset_path, batch_size=8)
+    # TODO: put the parsed argument for batch size
+    loader = DataLoader(arguments.dataset_path, image_shape=(224, 224, 3), batch_size=8)
     train_dataset = loader.get_train_dataset()
+    val_dataset = loader.get_val_dataset()
     num_classes = len(loader.get_classes())
 
     model = tf.keras.Sequential([
-        layers.experimental.preprocessing.Rescaling(1. / 255),
+        # layers.experimental.preprocessing.Rescaling(1. / 255),
         layers.Conv2D(32, 3, activation='relu'),
         layers.MaxPooling2D(),
         layers.Conv2D(32, 3, activation='relu'),
@@ -28,13 +30,15 @@ def main(arguments):
     ])
 
     model.compile(optimizer='adam', loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-    model.fit(train_dataset, epochs=2)
+    model.fit(train_dataset, validation_data=val_dataset, epochs=2, use_multiprocessing=True)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_path", type=str, default="resources/UCF")
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--epochs", type=int, default=5)
     args = parser.parse_args()
     main(args)
 
